ad353ff7827c56d9a494911661b5328d0a68437c
xygxegv <56009756+gxy812@users.noreply.github.com>
2025-03-22 15:14:52 +0800

    Copy code from tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,84 @@
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at
+  https://RandomNerdTutorials.com/esp32-web-bluetooth/ Permission is hereby
+  granted, free of charge, to any person obtaining a copy of this software and
+  associated documentation files. The above copyright notice and this permission
+  notice shall be included in all copies or substantial portions of the
+  Software.
+*/
 #include <Arduino.h>
+#include <BLE2902.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
 
-// put function declarations here:
+BLEServer *pServer = nullptr;
+BLECharacteristic *pDirectionCharacterisitic = nullptr;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
-void setup() {}
+// TODO : Generate UUIDS
+const char *SERVICE_UUID = "";
+const char *DIRECTION_CHARACTERISTIC_UUID = "";
+
+enum Direction { None, Forward, TurnLeft, TurnRight, Backward };
+
+Direction movingDirection = None;
+
+class ServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer *_) {
+        deviceConnected = true;
+        Serial.println("Connected!");
+    }
+
+    void onDisconnect(BLEServer *_) {
+        deviceConnected = false;
+        Serial.println("Disconnected!");
+        // restart advertising
+        pServer->startAdvertising();
+    }
+};
+
+class CharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *prop) {
+        auto value = prop->getValue();
+        if (value.length() <= 0)
+            return;
+        auto receivedValue = static_cast<int>(value[0]);
+        // TODO: Motor integration
+        switch (receivedValue) {
+        case None:
+            // motors to 0
+        case Forward:
+        // both motors spin
+        case TurnLeft:
+        case TurnRight:
+        case Backward:
+        // both motors reverse
+        default:
+            Serial.println("Strange bluetooth value!");
+        }
+    }
+};
+
+void setup() {
+    Serial.begin(115'200);
+    BLEDevice::init("Wall");
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallbacks());
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    pDirectionCharacterisitic = pService->createCharacteristic(
+      DIRECTION_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
+    pDirectionCharacterisitic->setCallbacks(new CharacteristicCallbacks());
+    pDirectionCharacterisitic->addDescriptor(new BLE2902());
+    pService->start();
+
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(false);
+    pAdvertising->setMinPreferred(0);
+    BLEDevice::startAdvertising();
+}
 
 void loop() {}
